d4f3fdde4608234a71e23bb81496cecc6c5a3c4a
kainy01 <kainykawai@gmail.com>
2023-04-20 19:22:54 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(arr) {
+let val= arr.find((element, index, array) => {
+    return element.result === 'W'
+  });
+  if (!!val ){
+    return val.year
+  }else return undefined
+}
